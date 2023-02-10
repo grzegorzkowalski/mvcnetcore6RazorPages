d386a7458bbbc2870e5747327ccce97db66ab1f2
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-02-10 14:13:39 +0100

    Identity all

--- FilmDB/FilmDB/Areas/CMS/Pages/AddToRole.cshtml.cs
+++ FilmDB/FilmDB/Areas/CMS/Pages/AddToRole.cshtml.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FilmDB.Areas.CMS.Pages
 {
+    [Authorize(Roles = "Admin")]
     public class AddToRoleModel : PageModel
     {
         private RoleManager<IdentityRole> _roleManager;
--- FilmDB/FilmDB/Areas/CMS/Pages/ListRoles.cshtml.cs
+++ FilmDB/FilmDB/Areas/CMS/Pages/ListRoles.cshtml.cs
@@ -1,24 +1,32 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FilmDB.Areas.CMS.Pages
 {
+    [Authorize(Roles = "Admin")]
     public class ListRolesModel : PageModel
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private UserManager<IdentityUser> _userManager;
 
-        public ListRolesModel(RoleManager<IdentityRole> roleManager)
+        public ListRolesModel(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         public List<IdentityRole> Roles { get; set; }
+        public List<string> UserRoles { get; set; }
 
         public string Text { get; set; }
-        public void OnGet(string? text)
+        public async Task OnGetAsync(string? text)
         {
             Roles = _roleManager.Roles.ToList();
             Text = text == null ? "" : text;
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var test  = await _userManager.GetRolesAsync(user);
+            UserRoles = (List<string>)test;
         }
 
         public void OnPost()
